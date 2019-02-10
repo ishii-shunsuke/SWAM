26a202b778246fcd858e82502590b6f631512eda
井上達朗 <t.inoue@inouetatsurounoMacBook-puro.local>
2019-02-10 15:40:03 +0900

    配送先ビュー作成とルート・コントローラ変更

--- app/controllers/users/shipping_addresses_controller.rb
+++ app/controllers/users/shipping_addresses_controller.rb
@@ -1,2 +1,13 @@
 class Users::ShippingAddressesController < ApplicationController
+	def new
+	end
+
+	def index
+	end
+
+	def edit
+	end
+
+	def update
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
 	root 'users#new'
+	get '/order/set_address', to:'users/orders#set_address'
+	get '/order/confirmation/:shipping_address_id', to:'users/orders#new'
+	get '/order/complete', to:'users/orders#show'
+	get '/shipping_address/index', to:'users/shipping_addresses#index'
+	get '/shipping_address/new', to:'users/shipping_addresses#new'
+	get '/shipping_address/:id/edit', to:'users/shipping_addresses#edit'
 end
