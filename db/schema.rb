fd4e1f2b16cff064261edbcc26f7e54a439a4164
PGYoshi <raitei5555@gmail.com>
2019-02-10 16:39:09 +0900

    usersテーブルマイグレーションファイル削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_09_090908) do
+ActiveRecord::Schema.define(version: 2019_02_10_072913) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email"
@@ -110,6 +110,8 @@ ActiveRecord::Schema.define(version: 2019_02_09_090908) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
     t.string "last_name"
     t.string "first_name"
     t.string "last_name_kana"
@@ -122,11 +124,14 @@ ActiveRecord::Schema.define(version: 2019_02_09_090908) do
     t.integer "prefectures"
     t.string "city"
     t.text "block"
-    t.string "password"
-    t.string "password_confirmation"
     t.boolean "deleted_at"
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
