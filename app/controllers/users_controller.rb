b6daeff88b19763c1418d40db4738a61032e1c2f
石井俊祐 <ishiishunyuu@ishiishunyuunoMacBook-puro.local>
2019-02-17 12:21:06 +0900

    [実装テストの為、プッシュ]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,33 @@ class UsersController < ApplicationController
 	end
 
 	def edit
+		@user = User.find(params[:id])
 	end
 
 	def update
 	end
 
-	def show
+	def create
+		user = User.new(user_params)
+		user.save
 	end
 
+
+	def show
+		@user = User.new
+		#@user = User.find(current_user.id)
+		#@cart = Cart.find(params[:id])
+        @carts = Cart.all
+	end
+
+	def destroy
+		user = User.find(params[:id])
+		user = destroy
+		redirect_to new_user_session_path
+	end
+
+    private
+	def user_params
+		params.require(:user).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :birthday, :gender, :phone_number, :email_adress, :post_number, :prefectures, :city, :block, :password, :password_confirmation)
+	end
 end
