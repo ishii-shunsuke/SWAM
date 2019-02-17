2b935fd506f3b5781a87c371b1e9bdf375ab761e
PGYoshi <raitei5555@gmail.com>
2019-02-17 16:42:01 +0900

    deviseビュー完成新規登録完成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+
+	before_action :configure_permitted_parameters, if: :devise_controller? #deviseコントローラが動いたら、configure_permitted_parametersを処理する。
+
+  protected
+
+  def configure_permitted_parameters #deviseでNameを登録するのに必要な記述
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :birthday, :gender, :phone_number, :post_number, :prefectures, :city, :block])
+  end
 end
+
--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,11 @@ ActiveRecord::Schema.define(version: 2019_02_14_093915) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
@@ -119,6 +124,14 @@ ActiveRecord::Schema.define(version: 2019_02_14_093915) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.string "last_name"
     t.string "first_name"
     t.string "last_name_kana"
@@ -126,15 +139,11 @@ ActiveRecord::Schema.define(version: 2019_02_14_093915) do
     t.date "birthday"
     t.integer "gender"
     t.string "phone_number"
-    t.string "email_address"
     t.string "post_number"
     t.integer "prefectures"
     t.string "city"
     t.text "block"
     t.boolean "deleted_at"
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
