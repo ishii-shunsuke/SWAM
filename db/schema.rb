f188f633390230923f633ea22a814fb83c5c1c2d
井上達朗 <t.inoue@inouetatsurounoMacBook-puro.local>
2019-02-17 13:28:21 +0900

    user.rb修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< Updated upstream
-ActiveRecord::Schema.define(version: 2019_02_14_093915) do
-=======
 ActiveRecord::Schema.define(version: 2019_02_16_090900) do
->>>>>>> Stashed changes
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -151,7 +147,7 @@ ActiveRecord::Schema.define(version: 2019_02_16_090900) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.datetime "deleted_at"
+    t.boolean "deleted_at"
     t.index ["deleted_at"], name: "index_users_on_deleted_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
